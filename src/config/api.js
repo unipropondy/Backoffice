b9904aae449f2a3366233d442dcc3beaa9e116e9
bavani18 <bavaniabavani18@gmail.com>
2026-04-24 10:17:20 +0530

    Initial push

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
- export const BASE_URL = "https://backoffice-production-fc6a.up.railway.app";
+ export const BASE_URL = "https://backoffice-production-937b.up.railway.app";
 // export const BASE_URL = "http://localhost:3000";
  
 // const BASE_URL = "https://smabackoffice-production-fc6a.up.railway.app";
