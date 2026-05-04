df56681a7fc60f0fd912ea393e7e1d153296622f
bavani18 <bavaniabavani18@gmail.com>
2026-05-04 16:12:18 +0530

    node modules

--- backend/routes/salesreportRoutes.js
+++ backend/routes/salesreportRoutes.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const sql = require("mssql");
-const puppeteer = require("puppeteer-core");
-const { poolPromise } = require("../db");
-
+const { sql, poolPromise } = require("../db");
 
 // ✅ Get company details from database
 const getCompanyDetails = async () => {
