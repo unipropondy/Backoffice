0c7515b4328a1b42ca980f6a5f6436d30eccc62f
bavani18 <bavaniabavani18@gmail.com>
2026-05-06 12:26:10 +0530

    merged

--- src/config/api.js
+++ src/config/api.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
- export const BASE_URL = "https://backoffice-production-937b.up.railway.app"; //std
 
- export const BASE_URL = "https://backoffice-production-80e7.up.railway.app"; //cafe19
-=======
 //  export const BASE_URL = "https://backoffice-production-937b.up.railway.app"; //std
 
-  export const BASE_URL = "https://backoffice-production-7a4c.up.railway.app"; //cafe19
->>>>>>> ae0bb45360c7ed26fa16f897f0672cd623f6352d
+ export const BASE_URL = "https://backoffice-production-80e7.up.railway.app"; //cafe19
+
 
 // export const BASE_URL = "http://localhost:3000";
  
