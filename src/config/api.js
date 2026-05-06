71df71d7b37c009d72c6e73e77153f9271f92d3a
bavani18 <bavaniabavani18@gmail.com>
2026-05-06 12:55:11 +0530

    node modules

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,5 @@
 
-//  export const BASE_URL = "https://backoffice-production-937b.up.railway.app"; //std
-
- export const BASE_URL = "https://backoffice-production-80e7.up.railway.app"; //cafe19
-
+ export const BASE_URL = "https://backoffice-production-937b.up.railway.app"; //std
 
 // export const BASE_URL = "http://localhost:3000";
  
