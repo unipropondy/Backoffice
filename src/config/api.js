8d7fb133aeb13d94d4451f39da9703ad824922ff
lokesh9976 <lokeshkrishnamoorthy2002@gmail.com>
2026-06-15 11:48:36 +0530

    Configure env variable for frontend API URL

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 
- export const BASE_URL = "https://vanndikadai.up.railway.app"; //std
+ export const BASE_URL = process.env.REACT_APP_API_URL || "https://vanndikadai.up.railway.app"; //std
 
 // export const BASE_URL = "http://localhost:3000";
  
