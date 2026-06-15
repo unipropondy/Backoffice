00fd15143a21ad6ac1027fa51967f6bb31b9b2d2
lokesh9976 <lokeshkrishnamoorthy2002@gmail.com>
2026-06-15 11:27:00 +0530

    Update BASE_URL to vanndikadai.up.railway.app

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 
- export const BASE_URL = "https://backoffice-production-f3fe.up.railway.app"; //std
+ export const BASE_URL = "https://vanndikadai.up.railway.app"; //std
 
 // export const BASE_URL = "http://localhost:3000";
  
