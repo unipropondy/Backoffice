4090cfa9d390d4c95fa856653e639b76e752f27d
bavani18 <bavaniabavani18@gmail.com>
2026-07-05 19:04:43 +0530

    new screen

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://backoffice-production-6317.up.railway.app/";
+export const BASE_URL = "https://backoffice-production-26ab.up.railway.app";
 
 // export const BASE_URL = "http://localhost:3000";
 
