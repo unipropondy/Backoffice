07feaeddb840adf066302b96ed8da0109f6d605b
bavani18 <bavaniabavani18@gmail.com>
2026-07-18 12:17:39 +0530

    PROMOCODE

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
-export const BASE_URL = "https://vanndikadai.up.railway.app";
+export const BASE_URL ="https://vanndikadai.up.railway.app";
 
 
 
 
-// export const API_BASE_URL = "http://localhost:3002";
+// export const BASE_URL = "http://localhost:3000";
