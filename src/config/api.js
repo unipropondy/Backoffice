a18bf5bab8f871c22559e7e0bb4e229f8e3b1fce
bavani18 <bavaniabavani18@gmail.com>
2026-08-17 10:53:20 +0530

    terminal change

--- src/config/api.js
+++ src/config/api.js
@@ -1,12 +1,9 @@
 
-<<<<<<< HEAD
-// export const BASE_URL ="https://backoffice-production-351f.up.railway.app";
-=======
-export const BASE_URL = "https://backoffice-production-e686.up.railway.app";
->>>>>>> b790150d1e61fa9832077af7750260523e0890e2
+ export const BASE_URL ="https://backoffice-production-351f.up.railway.app";
 
 
-export const BASE_URL = "http://localhost:3000";
+
+// export const BASE_URL = "http://localhost:3000";
 
 // const BASE_URL = "https://smabackoffice-production-fc6a.up.railway.app";
 
--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-// export const BASE_URL ="https://vanndikadai.up.railway.app";
-=======
-export const BASE_URL = "https://backoffice-production-e686.up.railway.app";
->>>>>>> b790150d1e61fa9832077af7750260523e0890e2
+
+
+export const BASE_URL ="https://backoffice-production-351f.up.railway.app";
 
 
 
-
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
